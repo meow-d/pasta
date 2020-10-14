10106a7522a20f5564b7537411e3784ac6e80760
purecatprsn <isaacwenyi@gmail.com>
2020-10-14 15:19:58 +0800

    a

--- bot/main.py
+++ bot/main.py
@@ -18,11 +18,11 @@ def filterFunc(x):
 
 # TODO error handling
 
-# import logging
+#import logging
 #logging.basicConfig(level=logging.INFO)
 
 # bot command prefix
-bot = commands.Bot(command_prefix='meow ')
+bot = commands.Bot(command_prefix='meow ', case_insensitive=True)
 
 @bot.event
 async def on_ready():
@@ -31,46 +31,56 @@ async def on_ready():
 # template
 '''
 @bot.command(help='')
-async def echo(ctx, arg):
+async def name(ctx, arg):
     await ctx.send(arg)
 '''
 
-@bot.command(help='aaaaa')
+@bot.command(help='pong')
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+@bot.command(help='make the bot say stupid things')
 async def say(ctx,*,text):
-    await ctx.send(str(text))
+    await ctx.send(text)
+
 
 @bot.command(help='echoooooo ᴇᴄʜᴏᴏᴏᴏᴏᴏ ᵉᶜʰᵒᵒᵒᵒᵒᵒ')
 async def echo(ctx):
     LastMessage = await ctx.history(limit=1, before=ctx.message).flatten()
     await ctx.send(LastMessage[0].content)
 
+
 @bot.command(help='owo i\'ll roll a dice for you')
 async def dice(ctx,faces):
-    if faces == "":
+    if faces == None:
         faces = 6   
     await ctx.send("you rolled a " + str(random.randrange(1,int(faces),1)))
 
-pastaHelp = '''
-when you\'re too lazy to even copy and paste
-use \'meow pasta list\' to list all
-'''
+
 @bot.command(help='when you\'re too lazy to even copy and paste')
-async def pasta(ctx,pastaName):
+async def pasta(ctx,*,pastaName):
     pastaCSV = csv.DictReader(open('pasta.csv', mode='r'))
-    if pastaName == "list":
-        embedText = discord.Embed(title='title', description='\n'.join(filter(filterFunc,pastaCSV.fieldnames)))
-        await ctx.send(embed = embedText)
-    else:
-        try:
-            for row in pastaCSV:
-                pastaContent = (row[pastaName])
-        except:
-            pastaContent = (f"sorry i can't find {pastaName}")
-        await ctx.send(pastaContent)
+    try:
+        for row in pastaCSV:
+            pastaContent = (row[pastaName])
+            await ctx.send(pastaContent)
+    except:
+        message = await ctx.send(f"sorry i can't find {pastaName}")
+        await message.delete()
+
+
+@bot.command(help='list of pasta(s)')
+async def pastalist(ctx):
+    pastaCSV = csv.DictReader(open('pasta.csv', mode='r'))
+    embedText = discord.Embed(description='\n'.join(filter(filterFunc,pastaCSV.fieldnames)))
+    await ctx.send(embed = embedText)
+
 
 @bot.command(help='nut')
 async def emoji(ctx): 
     await ctx.send("<:Pokimane:761513674056794112> <:lmao:761792146571264050> <a:dancin:762655521730986015> <a:dancin:762655521730986015> <a:dancin:762655521730986015> <a:dancin:762655521730986015> <a:dancin:762655521730986015> <a:dancin:762655521730986015>")
+
 
 @bot.command()
 async def play(ctx,link):
@@ -87,6 +97,7 @@ async def play(ctx,link):
     else:
         await ctx.send("Already playing song")
         return
+
 
 bot.run('NzYxNzcyNzQzNTQ0Nzk5MjUy.X3feJw.TIuokEm9mRCdVVBQmHOaTGhaLBI')
 keep_alive.keep_alive()
