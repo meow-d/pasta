ff4dc3771ceb76612bc70a13467817c39d8d5565
purecatprsn <isaacwenyi@gmail.com>
2020-10-29 22:45:30 +0800

    aaaadsfdghsifgwsigf d

--- bot/YTDLSource.py
+++ bot/YTDLSource.py
@@ -3,26 +3,24 @@ import asyncio
 import youtube_dl
 
 
-youtube_dl.utils.bug_reports_message = lambda: ''
+youtube_dl.utils.bug_reports_message = lambda: ""
 
 
 ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+    "format": "bestaudio/best",
+    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+    "restrictfilenames": True,
+    "noplaylist": True,
+    "nocheckcertificate": True,
+    "ignoreerrors": False,
+    "logtostderr": False,
+    "quiet": True,
+    "no_warnings": True,
+    "default_search": "auto",
+    "source_address": "0.0.0.0",  # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 
-ffmpeg_options = {
-    'options': '-vn'
-}
+ffmpeg_options = {"options": "-vn"}
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
@@ -33,17 +31,19 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
         self.data = data
 
-        self.title = data.get('title')
-        self.url = data.get('url')
+        self.title = data.get("title")
+        self.url = data.get("url")
 
     @classmethod
     async def from_url(cls, url, *, loop=None, stream=False):
         loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        data = await loop.run_in_executor(
+            None, lambda: ytdl.extract_info(url, download=not stream)
+        )
 
-        if 'entries' in data:
+        if "entries" in data:
             # take first item from a playlist
-            data = data['entries'][0]
+            data = data["entries"][0]
 
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        filename = data["url"] if stream else ytdl.prepare_filename(data)
         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
--- bot/main.py
+++ bot/main.py
@@ -9,139 +9,152 @@ import keep_alive
 import youtube_dl
 from YTDLSource import YTDLSource
 from googleapiclient.discovery import build
-#import logging
-#logging.basicConfig(level=logging.INFO)
+
+# import logging
+# logging.basicConfig(level=logging.INFO)
 
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
 # bot command prefix
-bot = commands.Bot(command_prefix='meow ', case_insensitive=True)
+bot = commands.Bot(command_prefix="meow ", case_insensitive=True)
+
 
 @bot.event
 async def on_ready():
 
-    print('connected to Discord as ' + bot.user.name + '\n')
+    print("connected to Discord as " + bot.user.name + "\n")
 
 
 # template
-'''
+"""
 @bot.command(help='')
 async def name(ctx, arg):
 
     await ctx.send(arg)
-'''
+"""
 
 
-@bot.command(help='ping pong ping pong ping ping pong')
+@bot.command(help="ping pong ping pong ping ping pong")
 async def ping(ctx):
 
-    await ctx.send('pong')
+    await ctx.send("pong")
 
 
-@bot.command(help='make the bot say stupid things')
-async def say(ctx,*,text):
+@bot.command(help="make the bot say stupid things")
+async def say(ctx, *, text):
 
     await ctx.send(text)
 
 
-@bot.command(help='echoooooo ᴇᴄʜᴏᴏᴏᴏᴏᴏ ᵉᶜʰᵒᵒᵒᵒᵒᵒ')
+@bot.command(help="echoooooo ᴇᴄʜᴏᴏᴏᴏᴏᴏ ᵉᶜʰᵒᵒᵒᵒᵒᵒ")
 async def echo(ctx):
 
     LastMessage = await ctx.history(limit=1, before=ctx.message).flatten()
     await ctx.send(LastMessage[0].content)
 
 
-@bot.command(help='owo i\'ll roll a dice for you')
-async def dice(ctx,faces=6):
+@bot.command(help="owo i'll roll a dice for you")
+async def dice(ctx, faces=6):
     try:
         faces = int(faces)
-        diceValue = random.randrange(1,faces,1)
+        diceValue = random.randrange(1, faces, 1)
         await ctx.send("you rolled a " + str(diceValue))
     except:
-        await ctx.send(ctx.author.mention + ' state a number larger than 1 you dumb dumb')
+        await ctx.send(
+            ctx.author.mention + " state a number larger than 1 you dumb dumb"
+        )
 
 
-@bot.command(help='when you\'re too lazy to even copy and paste')
-async def pasta(ctx,*,pastaName):
+@bot.command(help="when you're too lazy to even copy and paste")
+async def pasta(ctx, *, pastaName):
 
-    pastaCSV = csv.DictReader(open('bot/pasta.csv', mode='r'))
+    pastaCSV = csv.DictReader(open("bot/pasta.csv", mode="r"))
     try:
         for row in pastaCSV:
-            pastaContent = (row[pastaName])
+            pastaContent = row[pastaName]
             await ctx.send(pastaContent)
     except:
         embed_pastaError = discord.Embed(description=f"sorry i can't find {pastaName}")
-        message = await ctx.send(embed = embed_pastaError)
+        message = await ctx.send(embed=embed_pastaError)
         await message.delete(3)
 
 
-@bot.command(help='list of cpoypastas', aliases=['pl'])
+@bot.command(help="list of cpoypastas", aliases=["pl"])
 async def pastalist(ctx):
 
-    pastaCSV = csv.DictReader(open('bot/pasta.csv', mode='r'))
-    pastaNameList = '\n'.join(pastaCSV.fieldnames[1:])
-    embed_pastaList = discord.Embed(description = pastaNameList)
-    await ctx.send(embed = embed_pastaList)
+    pastaCSV = csv.DictReader(open("bot/pasta.csv", mode="r"))
+    pastaNameList = "\n".join(pastaCSV.fieldnames[1:])
+    embed_pastaList = discord.Embed(description=pastaNameList)
+    await ctx.send(embed=embed_pastaList)
 
 
-@bot.command(help='nut')
-async def emoji(ctx): 
+@bot.command(help="nut")
+async def emoji(ctx):
 
-    await ctx.send("<:Pokimane:761513674056794112> <:lmao:761792146571264050> <a:dancin:762655521730986015> <a:dancin:762655521730986015> <a:dancin:762655521730986015> <a:dancin:762655521730986015> <a:dancin:762655521730986015> <a:dancin:762655521730986015>")
+    await ctx.send(
+        "<:Pokimane:761513674056794112> <:lmao:761792146571264050> <a:dancin:762655521730986015> <a:dancin:762655521730986015> <a:dancin:762655521730986015> <a:dancin:762655521730986015> <a:dancin:762655521730986015> <a:dancin:762655521730986015>"
+    )
 
 
-@bot.command(help='join voice channel')
+@bot.command(help="join voice channel")
 async def join(ctx):
 
     try:
         channel = ctx.author.voice.channel
     except:
-        await ctx.send(ctx.author.mention + ' connect to a voice channel first you big dumb')
+        await ctx.send(
+            ctx.author.mention + " connect to a voice channel first you big dumb"
+        )
     else:
         if ctx.voice_client is not None:
             return await ctx.voice_client.move_to(channel)
         await channel.connect()
 
 
-@bot.command(aliases=['stop','s'])
+@bot.command(aliases=["stop", "s"])
 async def leave(ctx):
 
     await ctx.voice_client.disconnect()
 
 
-@bot.command(aliases=['pf'])
-async def aaaahh(ctx,filename):
+@bot.command(aliases=["pf"])
+async def aaaahh(ctx, filename):
 
     guild = ctx.guild
-    voice_client: discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
-    audio_source = discord.FFmpegPCMAudio('audio files/' + filename + '.mp3')
+    voice_client: discord.VoiceClient = discord.utils.get(
+        bot.voice_clients, guild=guild
+    )
+    audio_source = discord.FFmpegPCMAudio("audio files/" + filename + ".mp3")
     if not voice_client.is_playing():
         voice_client.play(audio_source, after=None)
 
 
 async def search(query):
-    youtube = build("youtube",'v3',developerKey=os.getenv('GOOGLE_API_KEY'))
-    search_response = youtube.search().list(
+    youtube = build("youtube", "v3", developerKey=os.getenv("GOOGLE_API_KEY"))
+    search_response = (
+        youtube.search()
+        .list(q=query, type="video", part="id,snippet", maxResults=1)
+        .execute()
+    )
+    return (
+        "https://www.youtube.com/watch?v="
+        + search_response["items"][0]["id"]["videoId"]
+    )
 
-        q=query,
-        type="video",
-        part="id,snippet",
-        maxResults=1
 
-    ).execute()
-    return ('https://www.youtube.com/watch?v=' + search_response['items'][0]['id']['videoId'])
-
-
-@bot.command(aliases=['p'])
+@bot.command(aliases=["p"])
 async def play(ctx, *, query):
 
     try:
         channel = ctx.author.voice.channel
     except:
-        await ctx.send(ctx.author.mention + ' connect to a voice channel first you big dumb')
+        await ctx.send(
+            ctx.author.mention + " connect to a voice channel first you big dumb"
+        )
     else:
         if ctx.voice_client is not None:
             return await ctx.voice_client.move_to(channel)
@@ -150,11 +163,13 @@ async def play(ctx, *, query):
         async with ctx.typing():
             url = await search(query)
             player = await YTDLSource.from_url(url, loop=ctx.bot.loop, stream=True)
-            ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+            ctx.voice_client.play(
+                player, after=lambda e: print("Player error: %s" % e) if e else None
+            )
 
-        embed_player = discord.Embed(description=f'**Now playing:** {player.title}')
+        embed_player = discord.Embed(description=f"**Now playing:** {player.title}")
         await ctx.send(embed=embed_player)
 
 
 keep_alive.keep_alive()
-bot.run(os.getenv('DISCORD_TOKEN'))
+bot.run(os.getenv("DISCORD_TOKEN"))
